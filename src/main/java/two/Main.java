f5e883931a151281a4d5707a8d364d294e5a970b
fogfore <yh.924@foxmail.com>
2020-01-03 16:32:33 +0800

    00

--- src/main/java/two/Main.java
+++ src/main/java/two/Main.java
@@ -1,65 +1,59 @@
-//package two;
-//
-//import java.util.*;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner s = new Scanner(System.in);
-//        int n = s.nextInt();
-//        int m = s.nextInt();
-//        LinkedList<Pair> list = new LinkedList<>();
-//        for (int i = 0; i < n; i++) {
-//            int cur = s.nextInt();
-//            int gap = s.nextInt();
-//            Pair pair = new Pair(cur, gap, i);
-//            list.add(pair);
-//        }
-//        int[] rst = new int[n];
-//        while (list.size() >= m) {
-//            Pair p1 = new Pair(0, 0, -1);
-//            Pair p2 = new Pair(0, 0, -1);
-//            for (Pair p : list) {
-//                if (p.cur > p1.cur) {
-//                    p2 = p1;
-//                    p1 = p;
-//                } else if (p.cur == p1.cur && p.gap > p1.gap) {
-//                    p1 = p;
-//                } else if (p.cur > p2.cur) {
-//                    p2 = p;
-//                } else if (p.cur == p2.cur && p.gap > p2.gap) {
-//                    p2 = p;
-//                }
-//            }
-//            if (p1.idx >= 0) rst[p1.idx] = -1;
-//            if (p2.idx >= 0) rst[p2.idx] = -1;
-//            list.remove(p1);
-//            list.remove(p2);
-//            for (Pair p : list) {
-//                p.cur = Math.max(p.cur - p.gap, 0);
-//            }
-//            Iterator<Pair> iterator = list.iterator();
-//            while (iterator.hasNext()) {
-//                Pair next = iterator.next();
-//                if (next.cur == 0) {
-//                    iterator.remove();
-//                    rst[next.idx] = 0;
-//                }
-//            }
-//        }
-//        for (int i = 0; i < rst.length; i++) {
-//            System.out.println(rst[i]);
-//        }
-//    }
-//
-//    static class Pair {
-//        int cur;
-//        int gap;
-//        int idx;
-//
-//        Pair(int cur, int gap, int idx) {
-//            this.cur = cur;
-//            this.gap = gap;
-//            this.idx = idx;
-//        }
-//    }
-//}
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        Pair[] pairs = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            int cur = s.nextInt();
+            int gap = s.nextInt();
+            pairs[i] = new Pair(cur, gap);
+        }
+        while (true) {
+            int i1 = -1, i2 = -1;
+            for (int i = 0; i < n; i++) {
+                if (pairs[i].cur > 0) {
+                    if (i1 == -1) {
+                        i1 = i;
+                    } else if (pairs[i].cur > pairs[i1].cur || (pairs[i].cur == pairs[i1].cur && pairs[i].gap > pairs[i1].gap)) {
+                        i2 = i1;
+                        i1 = i;
+                    } else if (i2 == -1) {
+                        i2 = i;
+                    } else if (pairs[i].cur > pairs[i2].cur || (pairs[i].cur == pairs[i2].cur && pairs[i].gap > pairs[i2].gap)) {
+                        i2 = i;
+                    }
+                }
+            }
+            if (i1 > -1) pairs[i1].cur = -1;
+            if (i2 > -1) pairs[i2].cur = -1;
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (pairs[i].cur > 0) {
+                    pairs[i].cur = Math.max(0, pairs[i].cur - pairs[i].gap);
+                }
+                if (pairs[i].cur > 0) {
+                    count++;
+                }
+            }
+            if (count < m) {
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(pairs[i].cur);
+        }
+    }
+
+    static class Pair {
+        int cur;
+        int gap;
+
+        Pair(int cur, int gap) {
+            this.cur = cur;
+            this.gap = gap;
+        }
+    }
+}
