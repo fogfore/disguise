fc294441d3b391aeb8963534397ec5c4535a8c64
fogfore <yh.924@foxmail.com>
2020-01-03 16:09:26 +0800

    00

--- src/main/java/two/Main.java
+++ src/main/java/two/Main.java
@@ -1,9 +1,6 @@
 package two;
 
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,13 +11,13 @@ public class Main {
         for (int i = 0; i < n; i++) {
             int cur = s.nextInt();
             int gap = s.nextInt();
-            Pair pair = new Pair(cur, gap, 0);
+            Pair pair = new Pair(cur, gap, i);
             list.add(pair);
         }
         int[] rst = new int[n];
-        while (list.size() < m) {
-            Pair p1 = new Pair(0, 0, 0);
-            Pair p2 = new Pair(0, 0, 0);
+        while (list.size() >= m) {
+            Pair p1 = new Pair(0, 0, -1);
+            Pair p2 = new Pair(0, 0, -1);
             for (Pair p : list) {
                 if (p.cur > p1.cur) {
                     p2 = p1;
@@ -33,17 +30,19 @@ public class Main {
                     p2 = p;
                 }
             }
-            rst[p1.idx] = -1;
-            rst[p2.idx] = -1;
+            if (p1.idx >= 0) rst[p1.idx] = -1;
+            if (p2.idx >= 0) rst[p2.idx] = -1;
             list.remove(p1);
             list.remove(p2);
             for (Pair p : list) {
                 p.cur = Math.max(p.cur - p.gap, 0);
             }
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i).cur == 0) {
-                    Pair remove = list.remove(i);
-                    rst[remove.idx] = 0;
+            Iterator<Pair> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                Pair next = iterator.next();
+                if (next.cur == 0) {
+                    iterator.remove();
+                    rst[next.idx] = 0;
                 }
             }
         }
